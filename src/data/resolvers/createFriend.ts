b93c1627728f4203319dcaa50eba504c810d62aa
Uche Chilaka <uche@larcity.com>
2018-10-27 19:33:29 -0400

    tweaked variable names to payload and x(ecutable)Payload

--- src/data/resolvers/createFriend.ts
+++ src/data/resolvers/createFriend.ts
@@ -3,8 +3,8 @@ import * as crypto from "crypto";
 import { Friend } from '../models';
 import memDb from '../memdb';
 
-export const createFriend = function (memPayload, payload) {
-    const { input } = _.isEmpty(memPayload) ? payload : memPayload;
+export const createFriend = function (payload, xPayload) {
+    const { input } = _.isEmpty(payload) ? xPayload : payload;
     let id = crypto.randomBytes(10).toString('hex');
     memDb[id] = input;
     return new Friend(id, input);
--- src/data/resolvers/getFriend.ts
+++ src/data/resolvers/getFriend.ts
@@ -2,8 +2,8 @@ import _ from "lodash";
 import { Friend } from '../models';
 import memDb from '../memdb';
 
-export const getFriend = (memPayload, payload) => {
-    const { id } = _.isEmpty(memPayload) ? payload : memPayload;
+export const getFriend = (payload, xPayload) => {
+    const { id } = _.isEmpty(payload) ? xPayload : payload;
     if (typeof id === 'string') {
         return new Friend(id, memDb[id]);
     }
